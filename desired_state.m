4ef4d917fe56efe29d37efaefc56c3a79af91d03
laukikm <34072180+laukikm@users.noreply.github.com>
2019-11-07 11:36:42 -0700

    Update desired_state.m

--- desired_state.m
+++ desired_state.m
@@ -8,9 +8,9 @@ y=1*cos(pi/20*t);
 ydot=1*pi/20*sin(pi/20*t);
 %Calculate vref and uref
 
-%keyboard();
-v=xdot;%sqrt(xdot^2+ydot^2);
-phi=ydot/V0;%atan(ydot/xdot);
+%Given the x and y derivatives, it is possible to obtain the velocity and heading as follows
+v=sqrt(xdot^2+ydot^2);
+phi=atan(ydot/xdot);
 
 xref=[x;y;v;phi];
 
