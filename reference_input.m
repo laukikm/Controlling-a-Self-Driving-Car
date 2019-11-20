785d32902c80cd873582136034aece14b4a6f7d6
laukikm <34072180+laukikm@users.noreply.github.com>
2019-11-20 16:04:31 -0700

    Update reference_input.m

--- reference_input.m
+++ reference_input.m
@@ -8,8 +8,10 @@ xref2=desired_state(t+dt);
 
 xdot_ref=(xref2-xref1)/dt;
 
+V0=xref1(3);
+
 u1=xdot_ref(3);
-u2=xdot_ref(4);
+u2=xdot_ref(4)/V0; %Refer to the model equations (Each system parameter is assumed to be unity)
 
 uref=[u1;u2];
 end
